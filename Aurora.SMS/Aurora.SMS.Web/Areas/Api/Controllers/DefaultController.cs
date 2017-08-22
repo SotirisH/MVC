f77dc163acf3a2aa4bb5a36953f0e2f35b4f9fc5
Sotirish <shatzis@alisnet.gr>
2017-08-22 21:46:04 +0700

    Consolidate NuGet Packages

---
+++ Aurora.SMS/Aurora.SMS.Web/Areas/Api/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aurora.SMS.Web.Areas.Api.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Api/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
